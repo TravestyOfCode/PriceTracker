d14a9c605686cdb51b956321524e373f457779b6
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-11 08:07:40 -0400

    Adds DBContext and User.

---
+++ PriceTracker.Data/AppDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PriceTracker.Data;
+
+internal class AppDBContext : IdentityDbContext<AppUser>
+{
+    public AppDBContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+    }
+}
