111538a39f0a16b4dd3f3449a8305fd9409a8f21
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-11 08:51:16 -0400

    Creates UnitOfMeasureModel.

---
+++ PriceTracker.Data/UnitOfMeasure/UnitOfMeasureModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceTracker.Data.UnitOfMeasure;
+
+public class UnitOfMeasureModel
+{
+    [Display(Name = "Id")]
+    public int Id { get; set; }
+
+    [Display(Name = "Name")]
+    [Required]
+    [MaxLength(16)]
+    public string Name { get; set; }
+
+    [Display(Name = "Abbreviation")]
+    [Required]
+    [MaxLength(8)]
+    public string Abbreviation { get; set; }
+
+    [Display(Name = "Conversion to Grams Ratio")]
+    [Required]
+    public decimal ConversionToGramsRatio { get; set; }
+}
