07e57f238e3c0b563167f9ddaa77fa4c98ab17e7
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-11 07:31:06 -0400

    Add project files.

---
+++ PriceTracker.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PriceTracker.Web.Models;
+
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
