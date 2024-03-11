32151cccb7875b5ee8c7a428477c813eb73dcbab
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-11 09:16:41 -0400

    Adds Result type for returning status and values.

---
+++ PriceTracker.Data/Results/IResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace PriceTracker.Data.Results;
+
+public interface IResult
+{
+    public HttpStatusCode StatusCode { get; }
+
+    public bool WasSuccess { get; }
+
+    public bool WasFailure { get; }
+
+    public object Value { get; }
+}
---
+++ PriceTracker.Data/Results/ResultT.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace PriceTracker.Data.Results;
+
+public class Result<T> : Result, IResult<T>
+{
+    public Result(HttpStatusCode statusCode, T value = default) : base(statusCode, value)
+    {
+    }
+
+    public new T Value => (T)base.Value;
+}
