9026a1ae1f96963f9fa7259eee36f36fa1e10e1e
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-12 12:06:48 -0400

    Adds data migrations.

---
+++ PriceTracker.Data/Migrations/20240312160607_Adds-Unit-Of-Measure.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddsUnitOfMeasure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UnitOfMeasure",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Abbreviation = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    ConversionToGramsRatio = table.Column<decimal>(type: "decimal(9,5)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnitOfMeasure", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UnitOfMeasure");
+        }
+    }
+}
