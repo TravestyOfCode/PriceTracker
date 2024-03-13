1bfa717d8509762df331528745b14c7acb7f9314
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-13 09:56:05 -0400

    Adds data migration.

---
+++ PriceTracker.Data/Migrations/20240313135546_Adds-Store.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddsStore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Store",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Store", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Store_Name",
+                table: "Store",
+                column: "Name",
+                unique: true)
+                .Annotation("SqlServer:Clustered", false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Store");
+        }
+    }
+}
