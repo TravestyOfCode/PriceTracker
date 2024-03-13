52fe0ea0e6bd8f9ab8fe1c11ddbe6d2d5979f943
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-13 09:56:56 -0400

    Adds Store command tests.

---
+++ PriceTracker.Testing/Store/Commands/CreateStoreTests.cs
@@ -0,0 +1,66 @@
+﻿using PriceTracker.Data.Store.Commands;
+
+namespace PriceTracker.Testing.Store.Commands;
+
+public class CreateStoreTests : IClassFixture<BaseTestFixture>, IAsyncLifetime
+{
+    private readonly BaseTestFixture _fixture;
+
+    public CreateStoreTests(BaseTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+
+
+    [Fact]
+    public async Task Create_With_Valid_Values_Is_Possible()
+    {
+        // Arrange        
+        var createCommand = new CreateStore() { Name = "Target" };
+
+        // Act
+        var result = await _fixture.SendAsync(createCommand);
+
+        // Assert
+        result.WasSuccess.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+        result.Value.Name.Should().Be(createCommand.Name);
+    }
+
+    [Fact]
+    public async Task Create_With_Duplicate_Name_Is_Not_Possible()
+    {
+        // Arrange 
+        await _fixture.AddAsync(new Data.Entity.Store() { Name = "Target" });
+        var createCommand = new CreateStore() { Name = "Target" };
+
+        // Act
+        var result = await _fixture.SendAsync(createCommand);
+
+        // Assert
+        result.WasSuccess.Should().BeFalse();
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        result.Errors.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public async Task Create_Without_Name_Is_Not_Possible()
+    {
+        // Arrange
+        var createCommand = new CreateStore() { Name = "" };
+
+        // Act
+        var result = await _fixture.SendAsync(createCommand);
+
+        // Assert
+        result.WasSuccess.Should().BeFalse();
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _fixture.ResetDatabase();
+    }
+}
---
+++ PriceTracker.Testing/Store/Commands/DeleteStoreTests.cs
@@ -0,0 +1,59 @@
+﻿using PriceTracker.Data.Store.Commands;
+using System.Collections.Generic;
+
+namespace PriceTracker.Testing.Store.Commands;
+
+public class DeleteStoreTests : IAsyncLifetime, IClassFixture<BaseTestFixture>
+{
+    private readonly BaseTestFixture _fixture;
+
+    private List<Data.Entity.Store> entities;
+
+    public DeleteStoreTests(BaseTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    public async Task InitializeAsync()
+    {
+        // Seed data
+        entities = new List<Data.Entity.Store>()
+        {
+            new Data.Entity.Store() { Name = "Target" },
+            new Data.Entity.Store() { Name = "Walmart" },
+            new Data.Entity.Store() { Name = "Amazon" }
+        };
+        await _fixture.AddRangeAsync(entities);
+    }
+
+    [Fact]
+    public async Task Delete_With_Valid_Id_Is_Possible()
+    {
+        // Assert
+        var command = new DeleteStore() { Id = entities[0].Id };
+
+        // Act
+        var result = await _fixture.SendAsync(command);
+
+        // Assert
+        result.WasSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Delete_With_Invalid_Id_Is_Not_Possible()
+    {
+        // Assert
+        var command = new DeleteStore() { Id = 0 };
+
+        // Act
+        var result = await _fixture.SendAsync(command);
+
+        // Assert
+        result.WasSuccess.Should().BeFalse();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _fixture.ResetDatabase();
+    }
+}
