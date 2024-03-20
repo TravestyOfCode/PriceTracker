a31fdf2741e152bbcda3ede1f9f9a20f5508f0a0
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-20 09:55:49 -0400

    Adds extension to copy errors to ModelStateDictionary.

--- PriceTracker.Data/Results/ResultT.cs
+++ PriceTracker.Data/Results/ResultT.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Net;
 
 namespace PriceTracker.Data.Results;
 
@@ -9,4 +10,30 @@ public class Result<T> : Result, IResult<T>
     }
 
     public new T Value => (T)base.Value;
+}
+
+public static class ResultExtenions
+{
+    public static void AddErrors(this ModelStateDictionary modelState, ModelStateDictionary errors)
+    {
+        if (modelState == null || errors == null)
+        {
+            return;
+        }
+
+        foreach (var entry in errors)
+        {
+            foreach (var error in entry.Value.Errors)
+            {
+                if (error.Exception != null)
+                {
+                    modelState.TryAddModelException(entry.Key, error.Exception);
+                }
+                if (!string.IsNullOrWhiteSpace(error.ErrorMessage))
+                {
+                    modelState.TryAddModelError(entry.Key, error.ErrorMessage);
+                }
+            }
+        }
+    }
 }
