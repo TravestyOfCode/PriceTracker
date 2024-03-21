d0c38413ff14948f541a9ac8e83439b03b177914
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-21 19:14:32 -0400

    Adds display attributes for Views.

--- PriceTracker.Data/UnitConversion/UnitConversionModel.cs
+++ PriceTracker.Data/UnitConversion/UnitConversionModel.cs
@@ -1,4 +1,5 @@
 ﻿using PriceTracker.Data.UnitOfMeasure;
+using System.ComponentModel.DataAnnotations;
 
 namespace PriceTracker.Data.UnitConversion;
 
@@ -6,13 +7,19 @@ public class UnitConversionModel
 {
     public int Id { get; set; }
 
+    [Display(Name = "Source Unit")]
     public int SourceUnitOfMeasureId { get; set; }
 
+    [Display(Name = "Source Unit")]
     public UnitOfMeasureModel SourceUnitOfMeasure { get; set; }
 
+    [Display(Name = "Destination Unit")]
     public int DestinationUnitOfMeasureId { get; set; }
 
+    [Display(Name = "Destination Unit")]
     public UnitOfMeasureModel DestinationUnitOfMeasure { get; set; }
 
+    [Display(Name = "Conversion Ratio")]
+    [DisplayFormat(DataFormatString = "{0:N5}", ApplyFormatInEditMode = true)]
     public decimal ConversionRatio { get; set; }
 }
