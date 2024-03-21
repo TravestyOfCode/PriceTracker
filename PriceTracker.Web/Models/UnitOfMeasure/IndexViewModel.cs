becb8b02fe031ffcb18d799062f7cf0a763f69be
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-21 11:18:53 -0400

    Adds UnitOfMeasure controller, views, and viewmodels.

---
+++ PriceTracker.Web/Models/UnitOfMeasure/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PriceTracker.Data.UnitOfMeasure;
+using PriceTracker.Data.UnitOfMeasure.Commands;
+using System.Collections.Generic;
+
+namespace PriceTracker.Web.Models.UnitOfMeasure;
+
+public class IndexViewModel
+{
+    public IEnumerable<UnitOfMeasureModel> UnitOfMeasureList { get; set; } = new List<UnitOfMeasureModel>();
+
+    public CreateUnitOfMeasure Request { get; set; } = new CreateUnitOfMeasure();
+}
