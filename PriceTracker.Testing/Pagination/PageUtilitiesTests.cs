3d55c18b30f0e798cd0b8c77d57bb33c748bc7a5
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-27 14:26:06 -0400

    Adds Start and End page calculation extension methods with testing.

---
+++ PriceTracker.Testing/Pagination/PageUtilitiesTests.cs
@@ -0,0 +1,154 @@
+﻿using PriceTracker.Data.Results;
+using PriceTracker.Web.Utilities;
+using System.Collections.Generic;
+
+namespace PriceTracker.Testing.Pagination;
+
+public class PageUtilitiesTests
+{
+    private class MockIPageQuery : IPagedQuery
+    {
+        public int Page { get; set; }
+        public int PerPage { get; set; }
+        public string SortBy { get; set; }
+        public SortOrder SortOrder { get; set; }
+    }
+
+    private MockIPageQuery mockQuery;
+
+    public PageUtilitiesTests()
+    {
+        mockQuery = new MockIPageQuery() { Page = 1, PerPage = 10 };
+
+    }
+
+    [Fact]
+    public void StartPage_With_Current_Page_1_Calculates_Correctly()
+    {
+        // Arrange
+        mockQuery.Page = 1;
+        var result = PagedResult<int>.Ok(new List<int>(), mockQuery, 100);
+
+        // Act
+
+        // Assert
+        result.StartPage().Should().Be(1);
+    }
+
+    [Fact]
+    public void StartPage_With_Current_Page_2_Calculates_Correctly()
+    {
+        // Arrange
+        mockQuery.Page = 2;
+        var result = PagedResult<int>.Ok(new List<int>(), mockQuery, 100);
+
+        // Act
+
+        // Assert
+        result.StartPage().Should().Be(1);
+    }
+
+    [Fact]
+    public void StartPage_With_Current_Page_4_Calculates_Correctly()
+    {
+        // Arrange
+        mockQuery.Page = 4;
+        var result = PagedResult<int>.Ok(new List<int>(), mockQuery, 100);
+
+        // Act
+
+        // Assert
+        result.StartPage().Should().Be(2);
+    }
+
+    [Fact]
+    public void StartPage_With_Current_Page_8_Calculates_Correctly()
+    {
+        // Arrange
+        mockQuery.Page = 8;
+        var result = PagedResult<int>.Ok(new List<int>(), mockQuery, 100);
+
+        // Act
+
+        // Assert
+        result.StartPage().Should().Be(6);
+    }
+
+    [Fact]
+    public void StartPage_With_Current_Page_10_Calculates_Correctly()
+    {
+        // Arrange
+        mockQuery.Page = 10;
+        var result = PagedResult<int>.Ok(new List<int>(), mockQuery, 100);
+
+        // Act
+
+        // Assert
+        result.StartPage().Should().Be(6);
+    }
+
+    [Fact]
+    public void EndPage_With_Current_Page_1_Calculates_Correctly()
+    {
+        // Arrange
+        mockQuery.Page = 1;
+        var result = PagedResult<int>.Ok(new List<int>(), mockQuery, 100);
+
+        // Act
+
+        // Assert
+        result.EndPage().Should().Be(5);
+    }
+
+    [Fact]
+    public void EndPage_With_Current_Page_2_Calculates_Correctly()
+    {
+        // Arrange
+        mockQuery.Page = 2;
+        var result = PagedResult<int>.Ok(new List<int>(), mockQuery, 100);
+
+        // Act
+
+        // Assert
+        result.EndPage().Should().Be(5);
+    }
+
+    [Fact]
+    public void EndPage_With_Current_Page_4_Calculates_Correctly()
+    {
+        // Arrange
+        mockQuery.Page = 4;
+        var result = PagedResult<int>.Ok(new List<int>(), mockQuery, 100);
+
+        // Act
+
+        // Assert
+        result.EndPage().Should().Be(6);
+    }
+
+    [Fact]
+    public void EndPage_With_Current_Page_8_Calculates_Correctly()
+    {
+        // Arrange
+        mockQuery.Page = 8;
+        var result = PagedResult<int>.Ok(new List<int>(), mockQuery, 100);
+
+        // Act
+
+        // Assert
+        result.EndPage().Should().Be(10);
+    }
+
+    [Fact]
+    public void EndPage_With_Current_Page_10_Calculates_Correctly()
+    {
+        // Arrange
+        mockQuery.Page = 10;
+        var result = PagedResult<int>.Ok(new List<int>(), mockQuery, 100);
+
+        // Act
+
+        // Assert
+        result.EndPage().Should().Be(10);
+    }
+}
---
+++ PriceTracker.Web/Utilities/PageUtilities.cs
@@ -0,0 +1,34 @@
+﻿using PriceTracker.Data.Results;
+using System;
+
+namespace PriceTracker.Web.Utilities;
+
+public static class PageUtilities
+{
+    public static int StartPage(this IPagedResult result, int pageWindow = 2)
+    {
+        if (result.Page <= 0 || result.TotalPages <= pageWindow)
+        {
+            return 1;
+        }
+
+        return Math.Max(1, result.Page - pageWindow);
+    }
+
+    public static int EndPage(this IPagedResult result, int pageWindow = 2)
+    {
+        if (result.Page <= 0)
+        {
+            return 1;
+        }
+
+        if (result.Page >= result.TotalPages || result.TotalPages <= (pageWindow * 2))
+        {
+            return result.TotalPages;
+        }
+
+        var startPage = result.StartPage(pageWindow);
+
+        return Math.Max(startPage + (pageWindow * 2), Math.Min(result.Page + pageWindow, result.TotalPages));
+    }
+}
